351d4ef2a50da48bd7aba985016d01ae0b5900c1
Unacceptium <145642963+Unacceptium@users.noreply.github.com>
2025-08-04 12:18:53 +0200

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,195 @@
+-- simple_nodes/init.lua
+
+-- Define a simple dirt-like node
+minetest.register_node("8segmentdisplayblocks:1i", {
+    description = "one",
+    tiles = {"1i.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:2z", {
+    description = "two",
+    tiles = {"2z.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:3", {
+    description = "three",
+    tiles = {"3.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:4", {
+    description = "four",
+    tiles = {"4.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:5s", {
+    description = "five",
+    tiles = {"5s.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:6", {
+    description = "six",
+    tiles = {"6.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:7", {
+    description = "seven",
+    tiles = {"7.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:8", {
+    description = "eight",
+    tiles = {"8.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:9", {
+    description = "nein",
+    tiles = {"9.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:0o", {
+    description = "zero",
+    tiles = {"0o.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:a", {
+    description = "letter",
+    tiles = {"a.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:b", {
+    description = "letter",
+    tiles = {"b.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:c", {
+    description = "letter",
+    tiles = {"c.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:d", {
+    description = "letter",
+    tiles = {"d.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:e", {
+    description = "letter",
+    tiles = {"e.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:f", {
+    description = "letter",
+    tiles = {"f.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:g", {
+    description = "letter",
+    tiles = {"g.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:h", {
+    description = "letter",
+    tiles = {"h.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:j", {
+    description = "letter",
+    tiles = {"j.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:k", {
+    description = "letter",
+    tiles = {"k.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:l", {
+    description = "letter",
+    tiles = {"l.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:m", {
+    description = "letter",
+    tiles = {"m.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:n", {
+    description = "letter",
+    tiles = {"n.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:p", {
+    description = "letter",
+    tiles = {"p.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:q", {
+    description = "letter",
+    tiles = {"q.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:r", {
+    description = "letter",
+    tiles = {"r.png"}, 
+    groups = {crumbly = 3}
+})
+
+
+minetest.register_node("8segmentdisplayblocks:t", {
+    description = "letter",
+    tiles = {"t.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:u", {
+    description = "letter",
+    tiles = {"u.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:v", {
+    description = "letter",
+    tiles = {"v.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:w", {
+    description = "letter",
+    tiles = {"w.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:x", {
+    description = "letter",
+    tiles = {"x.png"}, 
+    groups = {crumbly = 3}
+})
+
+minetest.register_node("8segmentdisplayblocks:y", {
+    description = "letter",
+    tiles = {"y.png"}, 
+    groups = {crumbly = 3}
+})
